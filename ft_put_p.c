ee20ef89ef66cd27cca9a11f5da12d96d784f166
Ula <ursz.kam@gmail.com>
2024-03-18 00:01:57 +0100

    add function for printing pointers

--- ft_put_p.c
+++ ft_put_p.c
@@ -10,4 +10,44 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_printf.h"
 
+static int ptr_len(uintptr_t n)
+{
+	int	i;
+    
+    if (n == 0)
+        return (1);
+    i = 0;
+	while (n)
+	{
+		n /= 16;
+		i++;
+	}
+	return (i);
+}
+
+static void ft_put_p_rec(uintptr_t n, char *hex)
+{
+	if (n <= 15)
+		ft_put_c(hex[n % 16]);
+	else
+	{
+		ft_put_p_rec(n / 16, hex);
+		ft_put_c(hex[n % 16]);
+	}
+}
+
+int	ft_put_p(void *p)
+{
+    uintptr_t n;
+    char *hex;
+    
+    if (!p)
+        return(ft_put_s("(nil)"));
+    n = (uintptr_t) p;
+    hex = "0123456789abcdef";
+    ft_put_s("0x");
+	ft_put_p_rec(n, hex);
+	return (2 + ptr_len(n));
+}
