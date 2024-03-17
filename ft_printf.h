1e41f93ad42fc8a932b4d2f62eb5382d34bb426a
Urszula Kaminska <ukaminsk@c3r1s1.42warsaw.pl>
2024-03-17 16:35:43 +0100

    docs: add prototypes

--- ft_printf.h
+++ ft_printf.h
@@ -15,7 +15,16 @@
 
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdarg.h>
 
-int	ft_printf(const char *, ...);
+int		ft_printf(const char *format, ...);
+int	ft_put_d(int i);
+int	ft_put_s(char *str);
+int	ft_put_c(int c);
+int	ft_put_x(int i, int c);
+int	ft_put_u(unsigned int u);
+int	ft_put_p(void p);
+size_t	ft_strlen(const char *s);
+
 
 #endif
