c2eba179d426ac60fc3c3b47c695d6381a46ea9e
Urszula Kaminska <ukaminsk@c3r1s1.42warsaw.pl>
2024-03-17 16:37:02 +0100

    add logic for printing different data types

--- ft_printf.c
+++ ft_printf.c
@@ -10,7 +10,46 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_printf(const char *, ...)
-{
+#include "ft_printf.h"
 
+static int	ft_print_arg(va_list args, int c)
+{
+	int	i;
+	
+	if (c == 'c')
+		i = ft_put_c(va_arg(args, int));
+	else if (c == 's')
+		i = ft_put_s(va_arg(args, char *));
+	else if (c == 'd' || c == 'i')
+		i = ft_put_d(va_arg(args, int));
+	else if (c == 'x' || c == 'X')
+		i = ft_put_x(va_arg(args, int), c);
+	else if (c == 'p')
+		i = ft_put_p(va_arg(args, void *));
+	else if (c == 'u')
+		i = ft_put_u(va_arg(args, unsigned int));
+	else if (c == '%')
+		i = ft_put_c(c);
+	return (i);
+}
+
+int	ft_printf(const char * format, ...)
+{
+	va_list args;
+	int	len;
+
+	va_start(args, format);
+	len = 0;
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			len += ft_print_arg(args, *(++format));
+		}
+		else
+			len += ft_putchar(*format);
+		format++;
+	}
+	va_end(args);
+	return (i);
 }
