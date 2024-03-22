9a70e4370a412329ea9fae10a9f95739368cfdac
Ula <ursz.kam@gmail.com>
2024-03-22 23:12:32 +0100

    refactor: add default len value

--- ft_printf.c
+++ ft_printf.c
@@ -15,7 +15,8 @@
 static int	ft_print_arg(va_list args, int c)
 {
 	int	i;
-	
+
+	i = -1;
 	if (c == 'c')
 		i = ft_put_c(va_arg(args, int));
 	else if (c == 's')
@@ -33,10 +34,10 @@ static int	ft_print_arg(va_list args, int c)
 	return (i);
 }
 
-int	ft_printf(const char * format, ...)
+int	ft_printf(const char *format, ...)
 {
-	va_list args;
-	int	len;
+	va_list	args;
+	int		len;
 
 	va_start(args, format);
 	len = 0;
@@ -47,9 +48,9 @@ int	ft_printf(const char * format, ...)
 			len += ft_print_arg(args, *(++format));
 		}
 		else
-			len += ft_putchar(*format);
+			len += ft_put_c(*format);
 		format++;
 	}
 	va_end(args);
-	return (i);
+	return (len);
 }
